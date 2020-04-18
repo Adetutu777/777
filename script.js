280254358d04db04fa0018ed753198218b1ba784
Adetutu777 <60041984+Adetutu777@users.noreply.github.com>
2020-04-18 22:22:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,28 +20,7 @@ function calculate(parameter){
 }
 
 calculate(100);
-
-
-
-function calculate(parameter){
-	calc=[];
-	for (var i = 1; i <= parameter; i++) {
-		if (i%2==0 && i%3==0  && i%5==0)
-		calc.push("yu-gi-oh");
-		else if (i%2==0 && i%3==0) 
-			calc.push("yu-gi");
-		else if(i % 2==0)
-			calc.push("yu");
-		else if (i % 3==0) 
-			calc.push("gi");
-		else if(i % 5 ==0 )
-			calc.push("oh");
-		else
-			calc.push(i);
-
-
-	}
-	console.log(calc);
-}
-
 calculate(50);
+
+
+
